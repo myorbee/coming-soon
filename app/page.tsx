58d33c55d8917cd99fcb03b493e059be3717037e
Goodness Lu <goodnesslu.dev@gmail.com>
2025-05-29 04:37:25 +0300

    add indicators to tabs

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ import TopBar from "@/components/TopBar";
 import EmailRow from "@/components/EmailRow";
 import EmailContent from "@/components/EmailContent";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default function Home() {
   const [isEmailOpen, setIsEmailOpen] = useState(false);
 
@@ -23,14 +30,36 @@ export default function Home() {
 
         <div className="flex-1 flex flex-col overflow-hidden">
           {/* Email category tabs */}
-          <div className="flex border-b border-orbee-gray-200 bg-white">
-            <div className="px-6 py-3 border-b-2 border-orbee-violet text-orbee-violet font-medium">
+          <div className="flex border-b border-orbee-gray-200 bg-white justify-between md:justify-normal md:gap-6">
+            <div className="px-6 py-3 border-b-2 border-orbee-violet text-orbee-violet font-medium relative">
               Primary
+              <span
+                className={`absolute top-3 -right-1 ${isEmailOpen ? "bg-orbee-gray-200 text-orbee-gray-500" : "bg-orbee-violet text-white"} text-xs rounded-full w-4 h-4 flex items-center justify-center`}
+              >
+                {isEmailOpen ? "0" : "1"}
+              </span>{" "}
             </div>
-            <div className="px-6 py-3 text-orbee-gray-500/60  cursor-pointer">
-              Promotions
-            </div>
-            <div className="px-6 py-3 text-orbee-gray-500/60  cursor-pointer">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <div className="px-6 py-3 -ml-3 text-orbee-gray-500/60  cursor-pointer relative">
+                    Unread
+                    <span
+                      className={`absolute top-3 -right-6 bg-orbee-gray-200 text-orbee-gray-500 text-xs rounded-full w-11 h-4 flex items-center justify-center`}
+                    >
+                      1,723+
+                    </span>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent side="right">
+                  <h2 className="font-bold text-muted-foreground">Oppss...</h2>
+                  <p className="text-xs text-muted-foreground/60">
+                    All the emails you said you'd get back to later
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <div className="px-6 py-3 mr-5 text-orbee-gray-500/60  cursor-pointer">
               Social
             </div>
           </div>
